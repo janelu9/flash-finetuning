f00f7805eac2f44f8638307a341276efc5545b7a
root <root@DESKTOP-P710SMF.localdomain>
2023-07-09 21:10:25 +0800

    add LoRA for efficient training

--- train_pipe.py
+++ train_pipe.py
@@ -6,15 +6,11 @@
 
 import torch
 import deepspeed
-import os
-import argparse
-import numpy as np
 from transformers import (
     LlamaConfig,
     SchedulerType,
     default_data_collator,
-    get_scheduler,
-)
+    get_scheduler,)
 from ds_utils import (
     print_rank_0,
     to_device,
@@ -26,14 +22,20 @@ from ds_utils import (
     load_hf_tokenizer,
     get_train_ds_config,
     PromptDataset,
-    PromptDataCollatorPipe
-)
+    PromptDataCollatorPipe)
+from lora import (
+    convert_linear_layer_to_lora,
+    convert_lora_to_linear_layer,
+    only_optimize_lora_parameters)
 from torch.utils.data import DataLoader
 from deepspeed.utils import RepeatingLoader
 from deepspeed.ops.adam import DeepSpeedCPUAdam, FusedAdam
 from deepspeed.runtime.pipe.topology import PipeModelDataParallelTopology
 from pipeline_llama import LlamaForCausalLMPipe,LlamaCrossEntropyLoss
+import numpy as np
 import pyarrow.parquet
+import os
+import argparse
 
 parser = argparse.ArgumentParser(description='My training script.')
 parser.add_argument('--local_rank', type=int, default=-1,
@@ -53,11 +55,23 @@ parser.add_argument('--steps_per_print',
 parser.add_argument('--gradient_checkpointing',
                     action='store_true',
                     help='Enable gradient checkpointing for model.')
+## LoRA for efficient training setting
+parser.add_argument("--lora_dim",
+                    type=int,
+                    default=0,
+                    help="If > 0, use LoRA for efficient training.")
+parser.add_argument("--lora_module_name",
+                    type=str,
+                    default= "self_attn,mlp",
+                    help="The scope of LoRA.")
+parser.add_argument('--only_optimize_lora',
+                    action='store_true',
+                    help='Only optimize the LoRA parameters.')
                     
 parser = deepspeed.add_config_arguments(parser)
 
 args=parser.parse_args()
-args.model_path = "decapoda-research/llama-micro-hf"
+args.model_path = "decapoda-research/llama-13b-hf"
 args.data_dir = "news-commentary-v13-zh-en_parquet"
 args.zero_stage=1
 args.num_train_epochs=1
@@ -73,6 +87,11 @@ args.pipe_parallel_size = 1
 args.model_parallel_size = 1
 args.gradient_checkpointing = True
 
+if args.gradient_checkpointing and args.lora_dim > 0:
+    assert (
+        not args.only_optimize_lora
+    ), "--gradient_checkpointing and --only_optimize_lora cannot be enabled at the same time."
+
 def main():
     if args.local_rank == -1:
         device = torch.device("cuda")
@@ -80,39 +99,45 @@ def main():
         torch.cuda.set_device(args.local_rank)
         device = torch.device("cuda", args.local_rank)
         deepspeed.init_distributed()
-    
     args.global_rank = torch.distributed.get_rank()
     args.world_size = torch.distributed.get_world_size()
     assert args.world_size % (args.pipe_parallel_size * args.model_parallel_size) == 0
     args.data_parallel_size = args.world_size // (args.pipe_parallel_size * args.model_parallel_size)
-    
-    ds_config = get_train_ds_config(offload=args.offload,
-                                stage=args.zero_stage,)
+
+    ds_config = get_train_ds_config(
+        offload=args.offload,
+        stage=args.zero_stage,)
     ds_config[
         'train_micro_batch_size_per_gpu'] = args.per_device_train_batch_size
     ds_config[
         'train_batch_size'] = args.per_device_train_batch_size*args.gradient_accumulation_steps*args.data_parallel_size
     ds_config['steps_per_print'] = args.steps_per_print
-    
     set_random_seed(args.seed)
     torch.distributed.barrier()
-    
+
     config=LlamaConfig.from_pretrained(args.model_path)
     topo = PipeModelDataParallelTopology(
-                                         num_pp = args.pipe_parallel_size,
-                                         num_mp = args.model_parallel_size,
-                                         num_dp = args.data_parallel_size
-                                         )
+        num_pp = args.pipe_parallel_size,
+        num_mp = args.model_parallel_size,
+        num_dp = args.data_parallel_size)
     args.seed = args.seed + topo.get_coord(args.global_rank).pipe
-    model = LlamaForCausalLMPipe(config,args.gradient_checkpointing,
-                               loss_fn=LlamaCrossEntropyLoss(),
-                               topology=topo,
-                               base_seed=args.seed,
-                               )                             
+    model = LlamaForCausalLMPipe(
+        config,
+        args.gradient_checkpointing,
+        loss_fn=LlamaCrossEntropyLoss(),
+        topology=topo,
+        base_seed=args.seed,)                             
     model.from_pretrained(args.model_path)
-    
+    if args.lora_dim > 0:
+        model = convert_linear_layer_to_lora(
+            model,
+            args.lora_module_name.split(","),
+            args.lora_dim)
+        if args.only_optimize_lora:
+            model = only_optimize_lora_parameters(model)
+
     optimizer_grouped_parameters = get_optimizer_grouped_parameters(
-            model, args.weight_decay)
+        model, args.weight_decay)
     AdamOptimizer = DeepSpeedCPUAdam if args.offload else FusedAdam
     optimizer = AdamOptimizer(optimizer_grouped_parameters,
                               lr=args.learning_rate,
@@ -122,35 +147,35 @@ def main():
     data_files = [f for f in os.listdir(data_dir) if f[-4:] != '.crc']
     num_train_batch =sum(
         np.ceil(float(open(os.path.join(data_dir,f)).read().strip())/args.per_device_train_batch_size/args.data_parallel_size)
-        for f in os.listdir(data_dir) if f[-4:] == '.crc'
-    ) 
+        for f in os.listdir(data_dir) if f[-4:] == '.crc') 
     num_update_steps_per_epoch = np.ceil(
-        num_train_batch / args.gradient_accumulation_steps 
-        )
+        num_train_batch / args.gradient_accumulation_steps )
     lr_scheduler = get_scheduler(
         name=args.lr_scheduler_type,
         optimizer=optimizer,
         num_warmup_steps=args.num_warmup_steps,
-        num_training_steps=args.num_train_epochs * num_update_steps_per_epoch,
-    )      
+        num_training_steps=args.num_train_epochs * num_update_steps_per_epoch,)      
     
-    engine, _, _, _ = deepspeed.initialize(
-            model=model,
-            optimizer=optimizer,
-            args=args,
-            config=ds_config,
-            lr_scheduler=lr_scheduler,)
+    engine, *_ = deepspeed.initialize(
+        args=args,
+        config=ds_config,
+        model=model,
+        optimizer=optimizer,
+        lr_scheduler=lr_scheduler,)
 
     for epoch in range(args.num_train_epochs):
         for data_file in data_files:
             data = pyarrow.parquet.read_table(os.path.join(data_dir,data_file))
-            train_dataset = PromptDataset({k:data[k].to_numpy().tolist() for k in data.column_names})
-            train_dataloader = DataLoader(train_dataset,
-                                          collate_fn=PromptDataCollatorPipe(),
-                                          num_workers = args.gradient_accumulation_steps,
-                                          shuffle=True,
-                                          drop_last=False,
-                                          batch_size=args.per_device_train_batch_size)
+            train_dataset = PromptDataset(
+                {k:data[k].to_numpy().tolist() 
+                 for k in data.column_names})
+            train_dataloader = DataLoader(
+                train_dataset,
+                collate_fn=PromptDataCollatorPipe(),
+                num_workers = args.gradient_accumulation_steps,
+                shuffle=True,
+                drop_last=False,
+                batch_size=args.per_device_train_batch_size)
             cur_num_train_bacth =len(train_dataloader)
             print_rank_0(
                 f"Beginning of Epoch {epoch+1}/{args.num_train_epochs}, Data file: {data_file}, Total Micro Batches: {cur_num_train_bacth}/{int(num_train_batch)}",
@@ -160,13 +185,14 @@ def main():
             train_iter = iter(train_loader)
             for step in range(cur_num_train_bacth):
                 loss = engine.train_batch(data_iter=train_iter)
-            # Evaluate perplexity on the validation set.
-            # perplexity = evaluation(model, eval_dataloader)
             del data
             del train_dataset
             del train_dataloader
             gc.collect()
+            
     if args.global_rank == 0:
+        print_rank_0('saving model ...', args.global_rank)
+        engine = convert_lora_to_linear_layer(engine)
         if not os.path.exist(args.output_dir):
             os.makedirs(args.output_dir)
         engine.save_fp16_model(args.output_dir)
--- train_zero.py
+++ train_zero.py
@@ -6,8 +6,6 @@
 
 import torch
 import deepspeed
-from transformers.deepspeed import HfDeepSpeedConfig
-from deepspeed.ops.adam import DeepSpeedCPUAdam, FusedAdam
 from ds_utils import (
     print_rank_0,
     to_device,
@@ -19,26 +17,23 @@ from ds_utils import (
     load_hf_tokenizer,
     get_train_ds_config,
     PromptDataset,
-    PromptDataCollator
-)
+    PromptDataCollator)
 from transformers import (
     LlamaForCausalLM,
     LlamaTokenizer,
     LlamaConfig,
     SchedulerType,
     default_data_collator,
-    get_scheduler,
-)
-# from peft import (
-    # prepare_model_for_int8_training,
-    # LoraConfig,
-    # get_peft_model,
-    # get_peft_model_state_dict,
-    # set_peft_model_state_dict,
-# )
-import numpy as np
+    get_scheduler,)
+from lora import (
+    convert_linear_layer_to_lora,
+    convert_lora_to_linear_layer,
+    only_optimize_lora_parameters)
+from transformers.deepspeed import HfDeepSpeedConfig
+from deepspeed.ops.adam import DeepSpeedCPUAdam, FusedAdam
 from torch.utils.data import DataLoader
 from torch.utils.data.distributed import DistributedSampler
+import numpy as np
 import os
 import gc
 import pyarrow.parquet
@@ -62,11 +57,22 @@ parser.add_argument('--steps_per_print',
 parser.add_argument('--gradient_checkpointing',
                     action='store_true',
                     help='Enable gradient checkpointing for model.')
-                    
+## LoRA for efficient training setting
+parser.add_argument("--lora_dim",
+                    type=int,
+                    default=0,
+                    help="If > 0, use LoRA for efficient training.")
+parser.add_argument("--lora_module_name",
+                    type=str,
+                    default= "self_attn,mlp",
+                    help="The scope of LoRA.")
+parser.add_argument('--only_optimize_lora',
+                    action='store_true',
+                    help='Only optimize the LoRA parameters.')                    
 parser = deepspeed.add_config_arguments(parser)
 
 args=parser.parse_args()
-args.model_path = "decapoda-research/llama-micro-hf"
+args.model_path = "decapoda-research/llama-13b-hf"
 args.data_dir = "news-commentary-v13-zh-en_parquet"
 args.zero_stage=3
 args.num_train_epochs=1
@@ -79,6 +85,11 @@ args.num_warmup_steps=500
 args.learning_rate=1e-4
 args.output_dir = "./output"
 args.gradient_checkpointing = True
+
+if args.gradient_checkpointing and args.lora_dim > 0:
+    assert (
+        not args.only_optimize_lora
+    ), "--gradient_checkpointing and --only_optimize_lora cannot be enabled at the same time."
 
 def main():
     if args.local_rank == -1:
@@ -98,46 +109,50 @@ def main():
     ds_config[
         'train_batch_size'] = args.per_device_train_batch_size * args.world_size * args.gradient_accumulation_steps
     ds_config['steps_per_print'] = args.steps_per_print
-    
     set_random_seed(args.seed)
     torch.distributed.barrier()
     
-    model=LlamaForCausalLM.from_pretrained(args.model_path)
+    model = LlamaForCausalLM.from_pretrained(args.model_path)
     dschf = HfDeepSpeedConfig(ds_config)
     print_rank_0(dschf,args.global_rank)
-    
+    if args.lora_dim > 0:
+        model = convert_linear_layer_to_lora(
+            model,
+            args.lora_module_name.split(","),
+            args.lora_dim)
+        if args.only_optimize_lora:
+            model = only_optimize_lora_parameters(model)
 
-    # Split weights in two groups, one with weight decay and the other not.
     optimizer_grouped_parameters = get_optimizer_grouped_parameters(
         model, args.weight_decay)
-
     AdamOptimizer = DeepSpeedCPUAdam if args.offload else FusedAdam
     optimizer = AdamOptimizer(optimizer_grouped_parameters,
                               lr=args.learning_rate,
                               betas=(0.9, 0.95))
+                              
     data_dir = args.data_dir
     data_files = [f for f in os.listdir(data_dir) if f[-4:] != '.crc']
     num_train_batch =sum(
-        np.ceil(float(open(os.path.join(data_dir,f)).read().strip())/args.per_device_train_batch_size/args.world_size)
-        for f in os.listdir(data_dir) if f[-4:] == '.crc'
-    ) 
+        np.ceil(float(open(os.path.join(data_dir,f)).read().strip())
+                /args.per_device_train_batch_size
+                /args.world_size)
+        for f in os.listdir(data_dir) if f[-4:] == '.crc')    
     num_update_steps_per_epoch = np.ceil(
-        num_train_batch / args.gradient_accumulation_steps 
-        )
+        num_train_batch / args.gradient_accumulation_steps )
     lr_scheduler = get_scheduler(
         name=args.lr_scheduler_type,
         optimizer=optimizer,
         num_warmup_steps=args.num_warmup_steps,
-        num_training_steps=args.num_train_epochs * num_update_steps_per_epoch,
-    )       
+        num_training_steps=args.num_train_epochs * num_update_steps_per_epoch,)       
     
-    model, _, _, _ = deepspeed.initialize(
-    model = model,
-    optimizer=optimizer,
-    args=args,
-    config=ds_config,
-    lr_scheduler=lr_scheduler,
-    dist_init_required=True)
+    model,*_ = deepspeed.initialize(
+        args=args,
+        config=ds_config,
+        model = model,
+        optimizer=optimizer,
+        lr_scheduler=lr_scheduler,
+        dist_init_required=True)
+        
     if args.gradient_checkpointing:
         model.gradient_checkpointing_enable()
     
@@ -173,11 +188,13 @@ def main():
     for epoch in range(args.num_train_epochs):
         for data_file in data_files:
             data = pyarrow.parquet.read_table(os.path.join(data_dir,data_file))
-            train_dataset = PromptDataset({k:data[k].to_numpy().tolist() for k in data.column_names})
-            train_dataloader = DataLoader(train_dataset,
-                                          collate_fn = PromptDataCollator(),
-                                          sampler = DistributedSampler(train_dataset),
-                                          batch_size = args.per_device_train_batch_size)
+            train_dataset = PromptDataset({k:data[k].to_numpy().tolist() 
+                                           for k in data.column_names})
+            train_dataloader = DataLoader(
+                train_dataset,
+                collate_fn = PromptDataCollator(),
+                sampler = DistributedSampler(train_dataset),
+                batch_size = args.per_device_train_batch_size)
             print_rank_0(
                 f"Beginning of Epoch {epoch+1}/{args.num_train_epochs}, Data File: {data_file}, Total Micro Batches: {len(train_dataloader)}/{int(num_train_batch)}",
                 args.global_rank)
@@ -205,10 +222,9 @@ def main():
             # print_rank_0(f"ppl: {perplexity}", args.global_rank)
         model.tput_timer.update_epoch_count()
     
-    
     if args.output_dir is not None:
         print_rank_0('saving the final model ...', args.global_rank)
-
+        model = convert_lora_to_linear_layer(model)
         if args.global_rank == 0:
             save_hf_format(model, args)
 
