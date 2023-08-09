700d3206ff41b193621c2416fe8089255a9ce7d0
root <root@DESKTOP-P710SMF.localdomain>
2023-08-09 14:04:44 +0800

    update

--- repartition.sh
+++ repartition.sh
@@ -1,25 +1,22 @@
 #! /bin/bash
-
+# @author Jian Lu 
+# @date 2023/8/9
 set -e
-
 cd $1
 cur_partitions=`ls`
-uuid=`head -1 /dev/urandom | od -x |head -1| awk '{print $2$3$4$5$6$7$8$9}'`
-
-for ((i=0;i<$2 ;i++))
+uuid=`head -1 /dev/urandom|md5sum|head -c32`
+for ((i=0;i<$2;i++))
 do
 	file=`printf "$uuid-part-%05d" $i`
 	rm -rf $file
 	mkdir $file
 done
-
 i=0
 for f in `find -name *.parquet|sort`
 do
 	mv $f `printf "$uuid-part-%05d/" $((i%$2))`
 	i=$((i+1))
 done
-
 rm -rf $cur_partitions 
 crc=`ls .*.crc`
 cat $crc|awk '{sum+=$1} END {print sum}'>.$uuid.crc
