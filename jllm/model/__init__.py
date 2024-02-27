14b8d4d3ed611f7df2bddf10d426629d7a6c3ab9
janelu9 <652031063@qq.com>
2024-02-27 16:28:12 +0800

    update

--- jllm/model/__init__.py
+++ jllm/model/__init__.py
@@ -1,9 +1,10 @@
-from transformers import LlamaConfig
+from transformers import LlamaConfig,Qwen2Config
 from .llama.pipeline_llama import LlamaForCausalLMPipe,LlamaCrossEntropyLoss
 from .baichuan.modeling_baichuan import BaichuanConfig
 from .baichuan.pipeline_baichuan import BaichuanForCausalLMPipe,BaichuanCrossEntropyLoss
 from .qwen.modeling_qwen import QWenConfig
 from .qwen.pipeline_qwen import QWenForCausalLMPipe,QWenCrossEntropyLoss,QWenMixedLoss,QWenForClassCausalLMPipe
+from .qwen2.pipeline_qwen2 import Qwen2ForCausalLMPipe,Qwen2CrossEntropyLoss
 from .lora import convert_linear_layer_to_lora,_z3_params_to_fetch,convert_lora_to_linear_layer,only_optimize_lora_parameters
 from .llama.parallel_llama import (
     LlamaForCausalLMPipe as LlamaForCausalLMParallel,
@@ -14,12 +15,14 @@ ModelConfig = {
     'LlamaForCausalLM':LlamaConfig,
     'QWenLMHeadModel': QWenConfig,
     'QWenForClassCausalLM': QWenConfig,
-    'BaichuanForCausalLM':BaichuanConfig
+    'BaichuanForCausalLM':BaichuanConfig,
+    'Qwen2ForCausalLM':Qwen2Config
 }
 
 ModelPipe = {
     'LlamaForCausalLM':LlamaForCausalLMPipe,
     'QWenLMHeadModel':QWenForCausalLMPipe,
+    'Qwen2ForCausalLM':Qwen2ForCausalLMPipe,
     'QWenForClassCausalLM':QWenForClassCausalLMPipe,
     'BaichuanForCausalLM':BaichuanForCausalLMPipe
 }
@@ -27,6 +30,7 @@ ModelPipe = {
 CrossEntropyLossPipe = {
     'LlamaForCausalLM':LlamaCrossEntropyLoss,
     'QWenLMHeadModel': QWenCrossEntropyLoss,
+    'Qwen2ForCausalLM': Qwen2CrossEntropyLoss,
     'QWenForClassCausalLM': QWenMixedLoss,
     'BaichuanForCausalLM':BaichuanCrossEntropyLoss
     }
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 import io
 
 project_name = "jllm"  
-version = "2.1.0" 
+version = "2.1.2" 
  
 setuptools.setup(
     name=project_name,
