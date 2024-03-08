264580fdd4fb37ea27d16e157eeb585b10075227
janelu9 <652031063@qq.com>
2024-03-08 14:49:22 +0800

    update

--- jllm/model/__init__.py
+++ jllm/model/__init__.py
@@ -4,6 +4,7 @@ from .qwen.pipeline_qwen import QWenForCausalLMPipe,QWenForClassCausalLMPipe
 from .qwen2.pipeline_qwen2 import Qwen2ForCausalLMPipe
 from .lora import convert_linear_layer_to_lora,_z3_params_to_fetch,convert_lora_to_linear_layer,only_optimize_lora_parameters
 from .llama.parallel_llama import LlamaForCausalLMPipe as LlamaForCausalLMParallel
+from .qwen2.parallel_qwen2 import Qwen2ForCausalLMPipe as Qwen2ForCausalLMParallel
 
 ModelPipe = {
     'LlamaForCausalLM':LlamaForCausalLMPipe,
@@ -15,5 +16,5 @@ ModelPipe = {
     
 ModelParallel = {
     'LlamaForCausalLM':LlamaForCausalLMParallel,
+    'Qwen2ForCausalLM':Qwen2ForCausalLMParallel
 }
-
