079234f292b57da0b60ba407d24581276d6d9bf6
janelu9 <652031063@qq.com>
2024-12-01 14:10:13 +0800

    update

--- jllm/utils.py
+++ jllm/utils.py
@@ -82,6 +82,8 @@ def set_random_seed(seed):
         np.random.seed(seed)
         torch.manual_seed(seed)
         torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
 
 
 def get_all_reduce_mean(tensor):
